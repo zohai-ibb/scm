cd30cce0d1219576fce16e345c82432f52d0a6b5
Suhaib Alam <suhaibalam142@gmail.com>
2025-09-11 11:41:37 +0530

    Setting up mailstrap functionality

--- src/main/java/com/scm/services/impl/EmailServiceImple.java
+++ src/main/java/com/scm/services/impl/EmailServiceImple.java
@@ -1,5 +1,46 @@
 package com.scm.services.impl;
 
-public class EmailServiceImple implements EmailServic{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.scm.services.EmailServices;
+
+@Service
+public class EmailServiceImple implements EmailServices {
+
+    @Autowired
+    private JavaMailSender eMailSender;
+
+    @Value("${spring.mail.properties.domain_name}")
+    private String domainName;
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom(domainName);
+        eMailSender.send(message);
+
+
+
+    }
+
+    @Override
+    public void sendEmailWithAttachment() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendEmailWithAttachment'");
+    }
+
+    @Override
+    public void sendEmailWithHtml() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendEmailWithHtml'");
+    }
 
 }
--- src/test/java/com/scm/ApplicationTests.java
+++ src/test/java/com/scm/ApplicationTests.java
@@ -1,13 +1,27 @@
 package com.scm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.scm.services.EmailServices;
 
 @SpringBootTest
 class ApplicationTests {
+
+	@Autowired
+	private EmailServices service;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void sendEmailTest(){
+		service.sendEmail(
+			"johaibansari002@gmail.com",
+			"Testing email services",
+			"Working on email service"
+		);
+	}
 }
